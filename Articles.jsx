59cd33b3ab5fbc6e8dc725bc515385b24a869614
Yasin YILDIZ <144591826+yasinfedev@users.noreply.github.com>
2023-12-14 16:21:25 +0300

    Create Articles.jsx

---
+++ Articles.jsx
@@ -0,0 +1,43 @@
+export default function Articles() {
+
+  return (
+      <>
+      
+        <div>
+          <h2>Prototürk CSS derslerine devam ediyorum!</h2>
+          <p>Merhaba, Tayfun Erbilen hoca tarafından anlatılan CSS derslerine tüm hızımla devam ediyorum. CSS'i öğrenerek daha iyi projeler yapabilmek olan amacımı gerçekleştirmek için çok çalışacağım.</p>
+        </div>
+        <hr/>
+        <div>
+          <h2>DevFest Konya'ya sayılı günler kaldı!</h2>
+          <p>Merhaba, Konya ilimizde düzenlenecek olan DevFest Konya etkinliği bu cumartesi gerçekleştirilecek. Katılmak için sabırsızlanıyorum.</p>
+        </div>
+        <hr/>
+        <div>
+          <h2>5.hafta ödevini yapıyorum!</h2>
+          <p>Merhaba, MEGA Yazılım akademisi eğitimim kapsamındaki 5.hafta ödevimi yapıyorum. Güzel bir proje olabilmesi için öğrendiklerimi iyice harmanlayarak kullanmaya çalışacağım.</p>
+        </div>
+        <hr/>
+        <div>
+          <h2>5.hafta derslerini tamamladım</h2>
+          <p>Merhaba, MEGA Yazılım Akademi kapsamındaki ders programının 5.haftasındaki dersleri tamamladım. Bu hafta React.js'ye giriş yaptık ve birçok yeni şey öğrendim. Bolca pratik yaparak kendimi geliştireceğim.</p>
+        </div>
+        <hr/>
+        <div>
+          <h2>MEGA Yazılım Akademisi'ndeyim!</h2>
+          <p>Merhaba, sözlü mülakat aşamasının ardından Meram Belediyesi ve Üretken Akademi işbirliği ile düzenlenen 10 haftalık MEGA Yazılım Akademisi'ne kabul adım. Çok sevinçliyim. Bu fırsatı en iyi şekilde değerlendireceğim.</p>
+        </div>
+        <hr/>
+        
+        
+
+
+
+      </>
+  )
+
+
+
+
+}
+
